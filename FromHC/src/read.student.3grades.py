c659a942feb4d6c8b5c8975e8ff8d282c7d3715d
ustcwhc <ustcwhc@gmail,com>
2019-01-01 17:44:00 -0800

    add first assignment from Haocheng

---
+++ FromHC/src/read.student.3grades.py
@@ -0,0 +1,5 @@
+# read student.3grades.txt file into a dictionary,
+# where key is the student's name, value is the list of 3 integers.
+# The input file has format of:
+# name,gradeOfMath,gradeOfLanguage,gradeOfEnglish
+

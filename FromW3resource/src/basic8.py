09f6859fe0e66f2fc02501f394200337c65ffbfb
ustcwhc <ustcwhc@gmail,com>
2019-01-02 22:19:20 -0800

    add basic8.py

---
+++ FromW3resource/src/basic8.py
@@ -0,0 +1,6 @@
+"""
+Write a Python program to display the first and last colors from the following list. 
+What could you do to print the last item if you don't want to know the length of the list?
+
+color_list = ["Red","Green","White" ,"Black"]
+"""

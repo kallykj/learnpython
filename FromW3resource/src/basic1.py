c043735d2c02955a118f0c4edef4e9b580e95997
Jing <kallykj@gmail.com>
2019-01-02 19:22:54 -0800

    jing1

--- FromW3resource/src/basic1.py
+++ FromW3resource/src/basic1.py
@@ -2,9 +2,16 @@
 1. Write a Python program to print the following string in a specific format (see the output). Try to use '\t', instead of ' ', to add large space before some lines.
 
 Twinkle, twinkle, little star,
-	How I wonder what you are! 
+	How I wonder what you are!
 		Up above the world so high,
-		Like a diamond in the sky. 
-Twinkle, twinkle, little star, 
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
 	How I wonder what you are!
 """
+
+firstline = "Twinkle, twinkle, little star,\n"
+secondline = "\tHow I wonder what you are!\n"
+thirdline = "\t\tUp above the world so high,\n"
+forthline = "\t\tLike a diamond in the sky.\n "
+fifthline = "\tHow I wonder what you are!"
+print(firstline, secondline, thirdline, forthline, firstline, fifthline)
--- FromW3resource/src/basic2.py
+++ FromW3resource/src/basic2.py
@@ -1,3 +1,5 @@
 """
 2. Write a Python program to get the Python version you are using.
 """
+import sys
+print(sys.version)

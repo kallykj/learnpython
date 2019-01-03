620a2a45a7100df78d01273383389c6edc8006a0
ustcwhc <ustcwhc@gmail,com>
2019-01-02 22:02:41 -0800

    add 3 problems in FromW3resource and 1 problem in FromHC

---
+++ FromHC/src/swap.py
@@ -0,0 +1,29 @@
+"""
+Implement a function that can swap values in a list by given specific index
+
+The output of following code is expected to be:
+
+Before swapping: 200 300
+After swaping: 300 200
+"""
+
+def swap(list, i, j)
+    """
+	swap values in list on indice of i and j
+	
+	list: a List
+	i: index of first value
+	j: index of last value
+	"""
+	
+	# implement your code here and then delete "pass" line
+	pass
+	
+
+# main
+list = [0, 100, 200, 300, 400]
+i = 2
+j = 3
+print("Before swapping:", list[i], list[j])
+swap(list, i, j)
+print("After swapping:", list[i], list[j])

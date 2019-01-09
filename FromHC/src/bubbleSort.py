28b1e53bd1ac5f4d55dab3138a6c317c8c361bd8
ustcwhc <ustcwhc@gmail,com>
2019-01-08 22:29:52 -0800

    add bubbleSort problem

---
+++ FromHC/src/bubbleSort.py
@@ -0,0 +1,29 @@
+"""
+Write a sorting function in python by yourself 
+(do not use the system's sorting function). 
+Make use of previous bubbleMin(list, startIdx) function
+to help you put the min values in the former positions, 
+so that you can sort a given list in ascending order.
+
+for example:
+original list: [3,2,1,5]
+after bubbleSort(list): [1,2,3,5]
+
+original list: [1,2,100,101]
+after bubbleSort(list): [1,2,100,101]
+"""
+
+def bubbleSort(list):
+	# implement your sorting function here and delete "pass"
+    pass
+    
+	
+list1 = [3,2,1,5]
+print("original list:", list1)
+bubbleSort(list1)
+print("after bubbleSort(list)", list1)
+
+list2 = [1,2,100,101]
+print("original list:", list2)
+bubbleSort(list2)
+print("after bubbleSort(list)", list2)
